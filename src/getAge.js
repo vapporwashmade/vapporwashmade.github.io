9540a564e5d264c8d437a05e29d464b58fe0b939
apoorv <vappor.washmade@gmail.com>
2020-08-13 20:36:29 +0530

    updated dates

---
+++ src/getAge.js
@@ -0,0 +1,10 @@
+function getAge() {
+    const dateObj = new Date();
+    const year = dateObj.getFullYear(),
+        date = dateObj.getDate(),
+        month = dateObj.getMonth(),
+        hour = dateObj.getHours(),
+        second = dateObj.getSeconds(),
+        millisecond = dateObj.getMilliseconds();
+    
+}
